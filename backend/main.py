a4d3d28d8ea37fc4426e5a8eb4fa43701ed006ec
Sai Ganesh <120133664+saiganesh2004@users.noreply.github.com>
2026-02-03 17:11:45 +0530

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
+from datetime import datetime
+import random
 
+app = FastAPI()
+
+@app.get("/live-sales")
+def live_sales(count: int = 100):
+    data = []
+    for _ in range(count):
+        data.append({
+            "amount": random.randint(100, 5000),
+            "time": datetime.now().isoformat()
+        })
+    return data
